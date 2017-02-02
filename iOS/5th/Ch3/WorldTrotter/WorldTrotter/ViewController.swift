265be6da0362f1f960c7e8b638bdf26a6cf6f814
618programmer <jshdev@Joshuas-MacBook-Pro.local>
2017-02-01 23:44:07 -0600

    Initial Checkin
    Setting up Bignerdranch section, ch3 will be first

---
+++ iOS/5th/Ch3/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Joshua Renth on 2/1/17.
+//  Copyright © 2017 Joshua Renth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
