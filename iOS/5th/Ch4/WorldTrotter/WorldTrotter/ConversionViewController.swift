c8f5cbe2e7155f2e86fa26fec02b723a213a303a
618programmer <joshuarenth@BJC11196419.local>
2017-03-14 22:52:30 -0500

    Ch4/5 complete
    ch4 and 5 uploaded. minor variations in code due to swift update and
    outdated book

---
+++ iOS/5th/Ch4/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Joshua Renth on 3/14/17.
+//  Copyright © 2017 Joshua Renth. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLable()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        //celsiusLabel.text = textField.text
+        
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = text
+//        } else {
+//            celsiusLabel.text = "???"
+//        }
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLable() {
+        if let value = celsiusValue {
+            //celsiusLabel.text = "\(value)"
+            celsiusLabel.text = numberFormatter.string(from: value as NSNumber)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        print("Current text: \(textField.text)")
+//        print("Replacement text: \(string)")
+//        
+//        return true
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
---
+++ iOS/5th/Ch5/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Joshua Renth on 3/14/17.
+//  Copyright © 2017 Joshua Renth. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        // Always call the super implementation of viewDidLoad
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+    
+}
