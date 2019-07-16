48db691223571d822dbcf5cc143041537fc99a63
Joshua R <JTR0611@STL42JTR010622.local>
2019-07-15 21:54:57 -0500

    awful condition

--- Kotlin/ch3/src/Game.kt
+++ Kotlin/ch3/src/Game.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 100
+    //var healthPoints = 100
+    var healthPoints = 89
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")
