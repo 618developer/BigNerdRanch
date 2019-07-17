20018663ed6309ce88bb43ec3aa606563a425042
Joshua R <JTR0611@STL42JTR010622.local>
2019-07-16 21:31:51 -0500

    when Expressions

--- Kotlin/ch3/src/Game.kt
+++ Kotlin/ch3/src/Game.kt
@@ -16,29 +16,40 @@ fun main(args: Array<String>) {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
     //if (healthPoints == 100) {
-    val healthStatus = if (healthPoints == 100) {
-        //println(name + " is in excellent condition!")
-        "is in excellent condition!"
-        //} else if (healthPoints >= 90) {
-    } else if (healthPoints in 90..99) {
-        //println(name + " has a few scratches.")
-        "has a few scratches"
-        //} else if (healthPoints >= 75) {
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
-            //println(name + " has some minor wounds but is healing quite quickly!")
+//    val healthStatus = if (healthPoints == 100) {
+//        //println(name + " is in excellent condition!")
+//        "is in excellent condition!"
+//        //} else if (healthPoints >= 90) {
+//    } else if (healthPoints in 90..99) {
+//        //println(name + " has a few scratches.")
+//        "has a few scratches"
+//        //} else if (healthPoints >= 75) {
+//    } else if (healthPoints in 75..89) {
+//        if (isBlessed) {
+//            //println(name + " has some minor wounds but is healing quite quickly!")
+//            "has some minor wounds but is healing quite quickly!"
+//        } else {
+//            //println(name + " has some minor wounds.")
+//            "has some minor wounds."
+//        }
+//        //} else if (healthPoints >= 15) {
+//    } else if (healthPoints in 15..74) {
+//        //println(name + " looks pretty hurt.")
+//        "looks pretty hurt."
+//    } else {
+//        //println(name + " is in awful condition!")
+//        "is in awful condition!"
+//    }
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
-            //println(name + " has some minor wounds.")
             "has some minor wounds."
         }
-        //} else if (healthPoints >= 15) {
-    } else if (healthPoints in 15..74) {
-        //println(name + " looks pretty hurt.")
-        "looks pretty hurt."
-    } else {
-        //println(name + " is in awful condition!")
-        "is in awful condition!"
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
     // Player status
